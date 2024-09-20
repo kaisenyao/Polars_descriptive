df627e32b53b8547677a9f03d61437eba655f1a0
Kaisen Yao <kaisenyao@Kaisens-MBP.wired.duke.local>
2024-09-20 15:47:46 -0400

    test

--- test_main.py
+++ test_main.py
@@ -8,10 +8,23 @@ from main import (
     save_comparison_report,
 )
 
+file_path = "salary.csv"
+
+
+def test_read_dataset_pd():
+    df_pd = read_dataset_pd(file_path)
+    assert df_pd is not None
+
+
+def test_read_dataset_pl():
+    df_pl = read_dataset_pl(file_path)
+    assert df_pl is not None
+
+
 if __name__ == "__main__":
-    file_path = "salary.csv"
     df_pd = read_dataset_pd(file_path)
     df_pl = read_dataset_pl(file_path)
+
     if df_pl is not None and df_pd is not None:
         stats_pd = generate_summary_pd(df_pd)
         stats_pl = generate_summary_pl(df_pl)
